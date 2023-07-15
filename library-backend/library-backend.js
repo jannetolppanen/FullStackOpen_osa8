6b5a64b3f268d55edb59e1ec1202dd7477a93c0e
Janne Tolppanen <janne.ks@gmail.com>
2023-07-15 18:36:26 +0300

    8.6 done

--- library-backend/library-backend.js
+++ library-backend/library-backend.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -112,6 +113,15 @@ type Author {
     allBooks(author:String, genre:String): [Book!]
     allAuthors: [Author!]
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -135,6 +145,17 @@ const resolvers = {
       return books.filter((book) => book.author === author.name).length
     },
   },
+  Mutation: {
+    addBook: (root, args) => {
+      if (!authors.some(author => author.name === args.author)) {
+        const author = {name: args.author, id: uuid()}
+        authors = authors.concat(author)
+      }
+      const book = {...args, id: uuid()}
+      books = books.concat(book)
+      return book
+    }
+  }
 }
 
 const server = new ApolloServer({
