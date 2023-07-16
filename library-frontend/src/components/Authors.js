747fbbd4e2b3d99e97837efd4bf2ab37c6eaf170
Janne Tolppanen <janne.ks@gmail.com>
2023-07-16 21:47:00 +0300

    8.12 done

--- library-frontend/src/components/Authors.js
+++ library-frontend/src/components/Authors.js
@@ -1,28 +1,50 @@
 import { ALL_AUTHORS, ALL_BOOKS, CHANGE_BIRTHDAY } from '../queries'
 import { useMutation, useQuery } from '@apollo/client'
 import { useState } from 'react'
+import Select from 'react-select'
 
 const Authors = () => {
-  const [ name, setName] = useState('')
-  const [ born, setBorn ] = useState('')
+  const [name, setName] = useState('')
+  const [born, setBorn] = useState('')
+
+  const customStyles = {
+    control: (provided) => ({
+      ...provided,
+      width: "250px"
+    })
+  }
 
   const result = useQuery(ALL_AUTHORS)
-  const [ editAuthor ] = useMutation(CHANGE_BIRTHDAY, {
-    refetchQueries: [{ query: ALL_AUTHORS}, {query: ALL_BOOKS}]
+  const [editAuthor] = useMutation(CHANGE_BIRTHDAY, {
+    refetchQueries: [{ query: ALL_AUTHORS }, { query: ALL_BOOKS }],
   })
+
+  const handleNameChange = (selectedOption) => {
+    setName(selectedOption.value)
+  }
 
   const submit = async (event) => {
     event.preventDefault()
-
-    editAuthor( { variables: {name, setBornTo: born}})
-
+    editAuthor({ variables: { name, setBornTo: born } })
     setName('')
     setBorn('')
   }
-  
-  if (result.loading)  {
+
+  if (result.loading) {
     return <div>loading...</div>
   }
+
+  // Make a list of all authors, filter the ones without birthyear and map them into an object that gets sent to React Select component
+  const allAuthors = result.data.allAuthors
+  const authorsWithoutBirthdate = allAuthors.filter(
+    (author) => author.born === null
+  )
+  const authorOptions = authorsWithoutBirthdate.map((author) => {
+    return {
+      value: author.name,
+      label: author.name,
+    }
+  })
 
   return (
     <div>
@@ -41,18 +63,13 @@ const Authors = () => {
               <td>{a.bookCount}</td>
             </tr>
           ))}
-          </tbody>
-        </table>
-          <h2>set birthyear</h2>
+        </tbody>
+      </table>
+      <h2>set birthyear</h2>
 
-          <form onSubmit={submit}>
-        <div>
-          name
-          <input
-            value={name}
-            onChange={({ target }) => setName(target.value)}
-          />
-        </div>
+      <form onSubmit={submit}>
+        <Select styles={customStyles} options={authorOptions} onChange={handleNameChange} />
+
         <div>
           born
           <input
@@ -60,11 +77,8 @@ const Authors = () => {
             onChange={({ target }) => setBorn(parseInt(target.value))}
           />
         </div>
-
-
         <button type='submit'>update author</button>
       </form>
-
     </div>
   )
 }
